8dffe39fb8d3c898029d2e9623899ac060132843
shawen17 <shawen022@yahoo.com>
2023-10-05 10:58:04 +0100

    adminForm

--- src/components/realEstate/AdminForm.jsx
+++ src/components/realEstate/AdminForm.jsx
@@ -125,7 +125,7 @@ const AdminForm = (props) => {
         Accept: "application/json",
       },
     };
-    console.log(config);
+
     if (props.status === "on") {
       try {
         await axios.post(
@@ -133,7 +133,7 @@ const AdminForm = (props) => {
           data,
           config
         );
-        console.log("in");
+
         setAdded(true);
         setInputs({});
         window.scrollTo({
